e6e370424e4d6d3988a782b93f4c5cc75ba6ac1a
Алена Микитюк <alenamikituk@Alena.local>
2021-07-13 09:10:20 +0300

    few changes

--- gallery-from-the-lesson.js
+++ gallery-from-the-lesson.js
@@ -1,68 +1,4 @@
-const galleryItems = [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
-];
+import galleryItems from "./app.js";
 
 let activeIndex = 0;
 
@@ -80,8 +16,9 @@ const makeGalleryMarkup = ({ preview, original, description }) =>
       href='${original}'
     >
       <img
+        loading="lazy"
         class="gallery__image"
-        src='${preview}'
+        data-src='${preview}'
         data-source='${original}'
         alt='${description}'
       />
@@ -92,11 +29,33 @@ const markup = galleryItems.map(makeGalleryMarkup);
 
 refs.gallery.insertAdjacentHTML("beforeend", markup.join(""));
 
+if ("loading" in HTMLImageElement.prototype) {
+  document.querySelectorAll('img[loading="lazy"]').forEach((img) => {
+    img.src = img.dataset.src;
+  });
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js";
+  document.body.appendChild(script);
+}
+
+const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+const onImageLoaded = (e) => {
+  e.currentTarget.classList.add("is-loaded");
+};
+
+lazyImages.forEach((img) => {
+  img.addEventListener("load", onImageLoaded, { once: true });
+});
+
 function onOpenModal(e) {
   e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return;
   }
+
   markup.forEach((el, index) => {
     if (el.includes(e.target.src)) {
       activeIndex = index;
@@ -110,13 +69,13 @@ function onOpenModal(e) {
 }
 
 function closeModal() {
-  refs.modal.classList.remove("is-open");
   window.removeEventListener("keydown", keyboardManipulation);
+  refs.modal.classList.remove("is-open");
   refs.modalImg.src = "#";
   refs.modalImg.alt = "";
 }
 
-function onModalClose(e) {
+function onCloseModal(e) {
   if (e.target.nodeName === "IMG") {
     return;
   }
@@ -150,4 +109,4 @@ function keyboardManipulation({ key }) {
 }
 
 refs.gallery.addEventListener("click", onOpenModal);
-refs.modal.addEventListener("click", onModalClose);
+refs.modal.addEventListener("click", onCloseModal);
